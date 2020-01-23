4f712646aa6c9206a1fafebb71d2546f6bcc3a89
iggyvolz <iggyvolz@gmail.com>
2020-01-23 00:18:00 -0500

    Correct path in parsing script

--- data/parse.php
+++ data/parse.php
@@ -1,3 +1,3 @@
 <?php
-$file=array_map("json_decode",array_map("base64_decode",array_map("trim", file(__DIR__."/data/votes"))));
+$file=array_map("json_decode",array_map("base64_decode",array_map("trim", file(__DIR__."/votes"))));
 var_dump($file);
